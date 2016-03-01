98486f02709999d536aa1e53182431ca1879be4d
IvanGong <gongshenghu@gmail.com>
2016-03-01 20:38:23 +0800

    Added Application File

---
+++ commonbase/src/main/java/com/ivangong/commonbase/BaseApp.java
@@ -0,0 +1,12 @@
+package com.ivangong.commonbase;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by gongshenghu on 16/3/1.
+ */
+public class BaseApp extends MultiDexApplication {
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
---
+++ weather/src/main/java/com/ivangong/weather/WeatherApp.java
@@ -0,0 +1,12 @@
+package com.ivangong.weather;
+
+import com.ivangong.commonbase.BaseApp;
+
+/**
+ * Created by gongshenghu on 16/3/1.
+ */
+public class WeatherApp extends BaseApp {
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
